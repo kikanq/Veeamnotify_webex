b50cedc80c318a36bb2a214e305a8849170f7683
kikanq <79723156+kikanq@users.noreply.github.com>
2024-01-20 08:29:00 +0100

    Update Veeamnotify_VM_webex.ps1

--- Veeamnotify_VM_webex.ps1
+++ Veeamnotify_VM_webex.ps1
@@ -5,7 +5,7 @@ $webhookUrlVM = "https://webexapis.com/v1/webhooks/incoming/ID webhook webex"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 
-$LastVMSession = Get-VBRBackupSession | Sort-Object CreationTime -Descending | Select-Object -First 1
+$LastVMSession = Get-VBRBackupSession | Sort-Object EndTime -Descending | Select-Object -First 1
 $VMBackupDuration = $LastVMSession.EndTime - $LastVMSession.CreationTime
 $VMBackupDurationFormatted = '{0:D2}:{1:D2}:{2:D2}' -f $VMBackupDuration.Hours, $VMBackupDuration.Minutes, $VMBackupDuration.Seconds
 
